45c060dc3c6a2659aa1920329ec4b4496bc2a06d
arfin <arfinchowdhuryarif7@gamil.com>
2021-10-20 13:06:18 +0600

    first commit

---
+++ App.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import {
+  StyleSheet,
+  Text,
+  View,
+} from 'react-native';
+const App = () => {
+  return (
+   <View>
+     <Text>Hey Man..........</Text>
+   </View>
+  );
+};
+
+const styles = StyleSheet.create({
+ 
+});
+
+export default App;
